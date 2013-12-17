952c3cbf756c63df3e044c00b9e6fb9f53a2b3f5
shirleylberry <shirleylberry@gmail.com>
2013-12-16 21:29:30 -0500

    Progress on Reservations

---
+++ src/Reservations.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Reservations {
+    private String name;
+    private int time;
+    private int numPeople;
+    Scanner scanner = new Scanner(System.in);
+    String[][] reservations = {
+            {},
+            {},
+            {}
+    };
+
+    public static void main(String[] args){
+
+    }
+
+    public Reservations(){
+
+    }
+
+    public static void listRes(){
+        for (int i = 0; i < .size(); ){
+
+        }
+    }
+
+}
